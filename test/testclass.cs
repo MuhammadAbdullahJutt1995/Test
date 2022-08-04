29d32d0d2ecdab669c982d3765611c261f8dfa63
MuhammadAbdullahJutt1995 <110610043+MuhammadAbdullahJutt1995@users.noreply.github.com>
2022-08-04 17:06:41 +0400

    Add files via upload

---
+++ test/testclass.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Test
+{
+    public class Program
+    {
+        
+ 
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                string path = Convert.ToString(args[0]);
+                string Index = Convert.ToString(args[1]);
+                string ToMatch = Convert.ToString(args[2]);
+                StartProcess(path, Index, ToMatch);
+            }
+            else if (args.Length >3)
+                Console.WriteLine("More than required Arguements Found.");
+            else
+            {
+                Console.WriteLine("No Command Line Arguement found or Arguements Missing.");
+            }
+
+            // Console.WriteLine("Hello world");
+        }
+        public static void StartProcess(string FilePath, string key, string ToMatch)
+        {
+            try
+            {
+                //For storing the Filnal result
+                List<string> result = new List<string>();
+
+                string[] CSVFileData = System.IO.File.ReadAllLines(FilePath);
+
+                    foreach (string z in CSVFileData)
+                    {
+                        string[] Fields = z.Split(',');
+                        if (MatchRecords(key, ToMatch, Fields))
+                        {
+                            result.Add(z);
+                        }
+                    }
+                    //Displaying Result
+                    if (result.Count > 0)
+                        result.ForEach(Console.WriteLine);
+                    else
+                        Console.WriteLine("No record found(It's case Sensitive Kindly look Carefully)");
+
+
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine("Error while Fetching Results File Not Found");
+            }
+        }
+        public static bool MatchRecords(string ToMatchindex, string ToMatchkey, string[] FetchedFields)
+        {
+            if (FetchedFields.Contains(ToMatchkey) && FetchedFields.Contains(ToMatchindex))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
